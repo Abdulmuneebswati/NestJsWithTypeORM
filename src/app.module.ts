9507c851dd1b136999e5f5c8b8bf87a890ae4359
Abdulmuneebswati <muneebswati00@gmail.com>
2025-02-01 23:34:21 +0500

    middleware added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './typeorm/entities/User';
-import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { Post } from './typeorm/entities/Post';
 import { Profile } from './typeorm/entities/Profile';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
+    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
@@ -24,10 +26,7 @@ import { AuthModule } from './auth/auth.module';
       database: process.env.DATABASE,
       entities: [User, Post, Profile],
       synchronize: true,
-      logging: true,
     }),
-    UsersModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,31 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  // RequestMethod,
+} from '@nestjs/common';
 import { UserController } from './contollers/users/users.controller';
 import { UsersService } from './services/users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/typeorm/entities/User';
 import { Profile } from 'src/typeorm/entities/Profile';
 import { Post } from 'src/typeorm/entities/Post';
+import { LoggerMiddleware } from 'src/middlewares/logger.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Profile, Post])],
   controllers: [UserController],
   providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('users');
+  }
+}
+// export class AppModule implements NestModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer
+//       .apply(LoggerMiddleware)
+//       .forRoutes({ path: 'cats', method: RequestMethod.GET });
+//   }
+// }
